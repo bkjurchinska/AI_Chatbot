159c63f765eb0a5640840ba98b4ed535ff00f10f
Bisera Kjurchinska <bkjurchins@constructor.university>
2026-04-10 16:49:28 +0200

    completed Prisma Setup

--- ai-chatbot/lib/prisma.ts
+++ ai-chatbot/lib/prisma.ts
@@ -1,10 +1,17 @@
-import "dotenv/config";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const prismaClientSingleton = () => {
+  const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL || "file:./dev.db" });
+  return new PrismaClient({ adapter });
+};
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
-const prisma = new PrismaClient({ adapter });
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
+} & typeof global;
 
-export default prisma ;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
